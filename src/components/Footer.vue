dfa50375082decdb22cb039d6643cb99918f2ecc
DmacMcgreg <davejmcg@gmail.com>
2019-06-26 08:19:35 -0400

    Initial work on Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Footer">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  .Footer {
+    max-width: 100vw;
+    height: 150px;
+    background: #979197;
+  }
+</style>
