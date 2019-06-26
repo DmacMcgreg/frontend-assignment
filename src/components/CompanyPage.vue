3e10357c7893f335f5055126305e3161834982d6
DmacMcgreg <davejmcg@gmail.com>
2019-06-26 08:20:42 -0400

    Adding Panel data

---
+++ src/components/CompanyPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    CompanyPage
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    background: black;
+    width: 200px;
+    height: 200px;
+  }
+</style>
