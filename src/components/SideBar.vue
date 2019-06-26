f4976defd6b3f349ba722e8f2d7852fe93cbad08
DmacMcgreg <davejmcg@gmail.com>
2019-06-26 08:20:20 -0400

    Adding SideBar component to toggle view states

---
+++ src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="SideBar">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/colors.scss';
+  .SideBar {
+    width: 150px;
+    height: 300px;
+    border: 1px solid $borderColor;
+  }
+</style>
