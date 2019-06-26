8bd3cf6f6117c8cca2d1ab94b9d9f6fb71d75417
DmacMcgreg <davejmcg@gmail.com>
2019-06-26 11:44:19 -0400

    Adding Company Pages

--- src/components/CompanyData.vue
+++ src/components/CompanyData.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    CompanyData
+  <div class="CompanyDataPage">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae facilisis erat, vehicula pharetra nisl. Vestibulum gravida sodales varius. Ut pulvinar lorem iaculis libero maximus suscipit. </p>
+    <form>
+      <label>
+        COMPANY NAME
+        <FormInputField />
+      </label>
+
+      <label>
+        COMPANY SPEND
+        <FormInputField />
+      </label>
+
+      <label>
+        COMPANY SPEND ABILITY
+        <FormInputField />
+      </label>
+
+      <label>
+        NOTES
+        <FormInputField :large="true" />
+      </label>
+    </form>
   </div>
 </template>
 
 <script>
+  import FormInputField from './FormInputField.vue'
   export default {
-    
+    components: {
+      FormInputField,
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/colors.scss';
+  .CompanyDataPage {
+    height: 600px;
+    padding: 8px 12px 12px 12px;
 
+     p {
+      margin-bottom: 18px;
+    }
+
+    label {
+      color: $textGreyLight;
+      font-size: 12px;
+      margin-bottom: 2px;
+
+      :nth-child(n) {
+        margin-bottom: 14px;
+      }
+    }
+  }
 </style>
--- src/components/CompanyPage.vue
+++ src/components/CompanyPage.vue
@@ -1,18 +1,30 @@
 <template>
   <div>
     CompanyPage
+    <span>Count is {{count}}</span>
+    <button @click="increment">Increment</button>
   </div>
 </template>
 
 <script>
   export default {
-    
+    computed: {
+      count () {
+        return this.$store.state.count
+      }
+    },
+
+    methods: {
+      increment() {
+        this.$store.commit('increment')
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   div {
-    background: black;
+    background: lightblue;
     width: 200px;
     height: 200px;
   }
