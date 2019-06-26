7100c38cce62f4721a1242e344f4a950a3ed84a3
DmacMcgreg <davejmcg@gmail.com>
2019-06-26 08:18:49 -0400

    Main Page, split into separate components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,41 @@
 <template>
-  <h1>Hello world!</h1>
+  <div class="Viewport">
+    <Header />
+    <MainPanel />
+    <Footer />
+  </div>
+  
 </template>
 
 <script>
-export default {};
+  import Header from './Header.vue'
+  import MainPanel from './MainPanel.vue'
+  import Footer from './Footer.vue'
+
+  export default {
+    data() {
+      return {
+          menus: [
+            'PROSPECT',
+            'ALERT',
+            'LEARN'
+          ]
+      }
+    },
+    components: {
+      Header,
+      MainPanel,
+      Footer
+    },
+  };
 </script>
+
+<style lang="scss">
+  @import '../styles/colors.scss';
+
+  .Viewport {
+    max-width: 100vw;
+    height: 100vh;
+    overflow-x: hidden;
+  }  
+</style>
