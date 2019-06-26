a11c479f0cb246294b5b07a0ad528cbe1c389c3e
DmacMcgreg <davejmcg@gmail.com>
2019-06-26 11:44:07 -0400

    Adding Vuex store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import 'Styles/main.scss';
 import Vue from 'vue';
 import HelloWorld from 'Components/HelloWorld.vue';
+import Vuex from 'vuex'
+import {createStore} from './store'
+
+Vue.use(Vuex)
+const store = createStore()
 
 new Vue({
+  store,
   render: createElement => createElement(HelloWorld),
   el: '#app'
 });
